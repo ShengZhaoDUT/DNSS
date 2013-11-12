8c2f6fbc34f418a126294164048cc729cf0b9ee6
zs <zs@zs-System-Product-Name.(none)>
2013-11-12 22:29:45 +0800

    commit first

--- src/MongoDBReader.java
+++ src/MongoDBReader.java
@@ -1,4 +1,11 @@
 
-public class MongoDBReader {
+public class MongoDBReader implements ReadImplement{
 
+	@Override
+	public boolean read() {
+		// TODO Auto-generated method stub
+		
+		return false;
+		
+	}
 }
